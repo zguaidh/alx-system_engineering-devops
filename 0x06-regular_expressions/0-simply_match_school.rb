d70d4fc080b944189ea31db178371914197c809a
habiba zguaid <zguaidhabiba@gmail.com>
2023-11-02 21:28:27 -0700

    task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,3 @@
 #!/usr/bin/env ruby
-# Get the argument from the command line
-args = ARGV[0]
-
-# Define the regular expression pattern
-regexp = /School/
-
-# Find all matches in the input
-matches = args.scan(regexp)
-
-# Join the matches together
-result = matches.join
-
-# Print the result or a new line if no match found
-if args =~ regexp
-  puts result
-else
-  puts
-end
+# regular expression must match School
+puts ARGV[0].scan(/School/).join
