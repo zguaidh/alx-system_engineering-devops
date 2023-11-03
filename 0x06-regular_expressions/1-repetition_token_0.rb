e1a56266caa63287c2127985bb8c868c95ec566a
habiba zguaid <zguaidhabiba@gmail.com>
2023-11-02 21:40:09 -0700

    task 1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,19 +1,7 @@
 #!/usr/bin/env ruby
-# Get the argument from the command line
-args = ARGV[0]
-
-# Define the regular expression pattern
-regexp = /hbt*n/
-
-# Find all matches in the input
-matches = args.scan(regexp)
-
-# Join the matches together
-result = matches.join
-
-# Print the result or a new line if no match found
-if args =~ regexp
-  puts result
-else
-  puts
-end
+# regular expression that will match this cases
+# hbttn
+# hbtttn
+# hbttttn
+# hbtttttn
+puts ARGV[0].scan(/hbt{2,5}n/).join
