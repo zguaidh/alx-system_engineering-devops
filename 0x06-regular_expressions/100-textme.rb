e7541f8d6a252c67923f84ab7a2d07fad8d23d1e
habiba zguaid <zguaidhabiba@gmail.com>
2023-11-02 23:14:01 -0700

    task 7

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#output: [SENDER],[RECEIVER],[FLAGS]
+#Sender phone num or name (including country code if present)
+#Receiver phone num or name (including country code if present)
+#The flags that were used
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
