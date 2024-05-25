b4b371a606d79ae6d279d2c8efa140fdd441863f
zguaidh <zguaidhabiba@gmail.com>
2024-05-25 09:36:02 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# script that, using this REST API, for a given employee ID,
-# returns information about his/her TODO list progress.
+"""
+script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 import requests
 from sys import argv
 
