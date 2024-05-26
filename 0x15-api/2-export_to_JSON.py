a3c618c1d60eec9d3eae7c506e93071e6817001e
zguaidh <zguaidhabiba@gmail.com>
2024-05-26 10:38:24 -0700

    pycodestyle fixed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -21,14 +21,13 @@ if __name__ == "__main__":
 
     username = user.get('username')
     json_file = f'{user_id}.json'
-    
+
     data = {user_id: []}
 
     for todo in todos:
         data[user_id].append({"task": todo.get("title"),
-            "completed": todo.get("completed"), 
-            "username": username
-        })
+                              "completed": todo.get("completed"),
+                              "username": username})
 
     with open(json_file, 'w', encoding='utf-8') as f:
         json.dump(data, f)
